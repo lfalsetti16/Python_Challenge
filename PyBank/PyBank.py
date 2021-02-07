5956ddeeb24e183230910df4fed824c4fd143ce4
lfalsetti16 <69165100+lfalsetti16@users.noreply.github.com>
2021-02-07 13:36:19 -0500

    Add files via upload

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -1,7 +1,8 @@
 import os
 import csv
 
-csvpath = os.path.join("Resources", "budget_data.csv")
+csvpath = os.path.join("Resources/budget_data.csv")
+txtpath = os.path.join("Analysis/budget_data.txt")
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -10,10 +11,42 @@ with open(csvpath) as csvfile:
     
     total_months = 0
     total_amount = 0
+    previous = 0
+    change = 0
+    change_list = []
+    maxchange = 0
+    maxdate = ""
+    minchange = 99999999
+    mindate = ""
 
+#loop through to find total of months 
     for row in csvreader:
         total_months= total_months +1
         total_amount= total_amount + int(row[1])
+        change=int(row[1])- previous  
+        change_list.append(change)  
+        previous = int(row[1])
 
-    print (total_amount)
-    print (total_months)
+        if change>maxchange:
+            maxchange=change
+            maxdate=row[0]
+
+        if change<minchange:
+            minchange=change
+            mindate=row[0]
+
+    average_change = sum (change_list[1:])/len (change_list[1:])
+
+    print(f"Total Months: {total_months}")
+    print(f"Total Amount: {total_amount}")   
+    # print (change_list)
+    print(f"Average Change: {average_change}")
+    print(f"Greatest Increase {maxchange}{maxdate}")
+    print(f"Greatest Decrease {minchange}{mindate}")
+
+with open(txtpath,'w') as txtwriter:  
+    txtwriter.write(f"Total Months: {total_months} \n")
+    txtwriter.write(f"Total Amount: {total_amount} \n")
+    txtwriter.write(f"Average Change: {average_change}\n")
+    txtwriter.write(f"Greatest Increase {maxchange}{maxdate}\n")
+    txtwriter.write(f"Greatest Decrease {minchange}{mindate}\n")
